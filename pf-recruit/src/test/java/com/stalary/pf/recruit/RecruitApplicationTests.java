d4d7a4fb696682ca44263272c1bb21f14423ba72
stalary <452024236@qq.com>
2019-01-01 13:47:14 +0800

    mode

---
+++ pf-recruit/src/test/java/com/stalary/pf/recruit/RecruitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.stalary.pf.recruit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecruitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
---
+++ pf-resume/src/main/java/com/stalary/pf/resume/ResumeApplication.java
@@ -0,0 +1,18 @@
+package com.stalary.pf.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ResumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeApplication.class, args);
+    }
+
+}
+
